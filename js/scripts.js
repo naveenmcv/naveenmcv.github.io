311c073ab59022db39fa9ef8ecaa4bf444b6f300
Naveen M <83032821+naveenmcv@users.noreply.github.com>
2023-05-29 00:59:35 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Leaflet Map
+
+var map = L.map('map').setView([12.9716, 77.5946], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
